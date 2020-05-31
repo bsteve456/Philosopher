6ecc97a8bd5d16e39d5c220882c2faaf0b94ba38
bsteve456 <stevealan1@protonmail.com>
2020-05-31 14:59:45 +0200

    add Makefile and philo_one

---
+++ philo_one/includes/philo_one.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_one.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blacking <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/31 14:52:41 by blacking          #+#    #+#             */
+/*   Updated: 2020/05/31 14:58:54 by blacking         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_ONE_H
+# define PHILO_ONE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <pthread.h>
+
+typedef struct	s_waiter
+{
+	int	nthread;
+	int	tdie;
+	int	teat;
+	int pnum;
+}				t_waiter;
+
+#endif
