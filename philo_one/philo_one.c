73903158a1c8fb71c290b43879b02edb93c6d1d5
bsteve456 <stevealan1@protonmail.com>
2020-06-08 21:09:54 +0200

    add longer sleep

--- philo_one/philo_one.c
+++ philo_one/philo_one.c
@@ -14,7 +14,7 @@ void	*dinner(void *arg)
 		fn = fork_number(waiter->id, 1, waiter->nthread);
 		pthread_mutex_lock(&(waiter->fork[fn]));
 		waiter->tdie2[waiter->id - 1] -= (time - waiter->last_eat[waiter->id - 1]);
-		usleep(100);
+		usleep(1000);
 		if(ft_check_die_eat(waiter, time, 1) == 1)
 			break;
 	//	usleep(100);
@@ -22,28 +22,29 @@ void	*dinner(void *arg)
 		fn = fork_number(waiter->id, 2, waiter->nthread);
 		pthread_mutex_lock(&(waiter->fork[fn]));
 		waiter->tdie2[waiter->id - 1] -= (time - waiter->last_eat[waiter->id - 1]);
-		usleep(100);
+		usleep(1000);
 		if(ft_check_die_eat(waiter, time, 2) == 1)
 			break;
 		printf("%d %d has taken a fork\n", time, waiter->id);
 		waiter->last_eat[waiter->id - 1] = time;
+		printf("%d %d is eating\n", waiter->last_eat[waiter->id - 1], waiter->id);
 	//	usleep(100);
 		waiter->tdie2[waiter->id - 1] = waiter->tdie;
 		waiter->nb_eat[waiter->id - 1] += 1;
-		fn = fork_number(waiter->id, 2, waiter->nthread);
-		pthread_mutex_unlock(&(waiter->fork[fn]));
 		fn = fork_number(waiter->id, 1, waiter->nthread);
+		pthread_mutex_unlock(&(waiter->fork[fn]));
+		fn = fork_number(waiter->id, 2, waiter->nthread);
 		pthread_mutex_unlock(&(waiter->fork[fn]));
 		time += (waiter->last_eat[waiter->id - 1] + waiter->teat - time);
 		waiter->tdie2[waiter->id - 1] -= (time - waiter->last_eat[waiter->id - 1]);
-		usleep(100);
+		usleep(waiter->teat * 1000);
 		if(ft_check_die_eat(waiter, time, 0) == 1)
 			break;
 		printf("%d %d is sleeping\n", waiter->last_eat[waiter->id - 1] + waiter->teat, waiter->id);
 	//	usleep(100);
 		time += (waiter->last_eat[waiter->id - 1] + waiter->teat + waiter->tsleep - time);
 		waiter->tdie2[waiter->id - 1] -= (time - waiter->last_eat[waiter->id - 1]);
-		usleep(100);
+		usleep(waiter->tsleep  * 1000);
 		if(ft_check_die_eat(waiter, time, 0) == 1)
 			break;
 		printf("%d %d is thinking\n",waiter->last_eat[waiter->id - 1] + waiter->teat + waiter->tsleep, waiter->id);
