b79ec7b1250d875e4df6a4d5f8f5c990da66a089
bsteve456 <stevealan1@protonmail.com>
2020-06-14 19:30:23 +0200

    philo_three test

--- philo_three/philo_three.c
+++ philo_three/philo_three.c
@@ -1,33 +1,57 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <signal.h>
 
-void	dotest(int i)
+
+void	dotest(int n, sem_t *sem)
 {
-	int n = 0;
+	int i;
+
+	i = 0;
+	usleep(10000);
 	while(1)
 	{
-		printf("process number : %d, pid  %d\n", i, getpid());
-//		if(n == 100)
-//			break;
-//		n++;
+		printf("process number : %d\n", n);
+		if(n == 3 && i > 100)
+			break;
+		usleep(10000);
+		i++;
 	}
 }
 
 int main()
 {
 	int i = 0;
-	pid_t pids[3];
+	pid_t pid[5];
+	sem_t *sem;
 
-	while(i < 3)
+	sem_unlink("/test");
+	sem = sem_open("/test", O_CREAT, S_IRWXU, 1);
+	while (i < 5)
 	{
-		pids[i] = fork();
-		if(pids[i] == 0)
+		pid[i] = fork();
+		if(pid[i] == 0)
 		{
-			dotest(i);
-			exit(0);
+			dotest(i, sem);
+			exit(2);
 		}
 		i++;
 	}
+	int status;
+	waitpid(-1, &status, 0);
+	i = 0;
+	while(i < 5)
+	{
+		kill(pid[i], SIGTERM);
+		kill(pid[i], SIGKILL);
+		i++;
+	}
+	printf("status %d\n", status);
 
 }
