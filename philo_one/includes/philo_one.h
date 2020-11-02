269e0d2e725d1aa673e716f992346bd76c54927f
bsteve456 <stevealan1@protonmail.com>
2020-11-02 12:44:54 +0100

    norm philo_one.h

--- philo_one/includes/philo_one.h
+++ philo_one/includes/philo_one.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo_one.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: blacking <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: stbaleba <stbaleba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/05/31 14:52:41 by blacking          #+#    #+#             */
-/*   Updated: 2020/10/21 10:36:48 by stbaleba         ###   ########.fr       */
+/*   Created: 2020/11/02 12:35:20 by stbaleba          #+#    #+#             */
+/*   Updated: 2020/11/02 12:43:11 by stbaleba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,61 +21,61 @@
 
 # include <string.h>
 # define RESET 10000
-# define LEFT(X, Y)  (X % Y)
-# define RIGHT(X) (X - 1)
-typedef struct	s_msg
+
+typedef struct		s_msg
 {
 	int					msg;
 	long				time;
-}				t_msg;
+}					t_msg;
 
-typedef struct	s_waiter
+typedef struct		s_waiter
 {
-	int				nthread;
+	int					nthread;
 	long				tdie;
-	unsigned int				teat;
-	unsigned int				tsleep;
-	int				ntoeat;
+	unsigned int		teat;
+	unsigned int		tsleep;
+	int					ntoeat;
 	long				*tdie2;
 	long				*nb_eat;
 	long				*last_eat;
-	long					*display2;
-	pthread_mutex_t			*fork;
-	pthread_mutex_t			*display;
-	int				id;
-	int				j;
-	int				*fn;
-	int				*dead;
-	t_msg			**msg;
-	char			**tab;
-}				t_waiter;
-long			*ft_tdie(int nthread, long tdie);
-long			*ft_nb_eat(int nthread);
-long			*ft_last_eat(int nthread);
-long			**init_tab(char **av);
-t_waiter		*init_waiter(char **av, long **tab, int i, int ac);
-int			fork_number(int id, int pos, int last);
-int			ft_atoi(const char *str);
-long			utime(void);
+	long				*display2;
+	pthread_mutex_t		*fork;
+	pthread_mutex_t		*display;
+	int					id;
+	int					j;
+	int					*fn;
+	int					*dead;
+	t_msg				**msg;
+	char				**tab;
+}					t_waiter;
+long				*ft_tdie(int nthread, long tdie);
+long				*ft_nb_eat(int nthread);
+long				*ft_last_eat(int nthread);
+long				**init_tab(char **av);
+t_waiter			*init_waiter(char **av, long **tab, int i, int ac);
+int					fork_number(int id, int pos, int last);
+int					ft_atoi(const char *str);
+long				utime(void);
 pthread_mutex_t		*init_mutex(int n);
-int			philo_state(t_waiter *waiter, int thread, long time);
-void			ft_putnbr(long n);
-void			ft_putstr(char *s);
-void			dis_utils(long time, int id);
-int			lock_fork(t_waiter *waiter);
-int			lock_fork2(t_waiter *waiter);
-int			unlock_fork(t_waiter *waiter);
-void			has_afork(int id, long time);
-void			is_eating(int id, long time);
-void			is_sleeping(int id, long time);
-void			is_thinking(int id, long time);
-void			is_dead(int id, long time);
-void			display_all_msg(t_waiter *waiter, long time);
-void			display_msg(int id, int msg, long time, t_waiter *waiter);
-void			*ft_calloc(size_t count, size_t size);
-t_msg			*fill_msg();
-void			usleep_eat(t_waiter *waiter);
-void			usleep_sleep(t_waiter *waiter);
-void			usleep_ntime(int n);
+int					philo_state(t_waiter *waiter, int thread, long time);
+void				ft_putnbr(long n);
+void				ft_putstr(char *s);
+void				dis_utils(long time, int id);
+int					lock_fork(t_waiter *waiter);
+int					lock_fork2(t_waiter *waiter);
+int					unlock_fork(t_waiter *waiter);
+void				has_afork(int id, long time);
+void				is_eating(int id, long time);
+void				is_sleeping(int id, long time);
+void				is_thinking(int id, long time);
+void				is_dead(int id, long time);
+void				display_all_msg(t_waiter *waiter, long time);
+void				display_msg(int id, int msg, long time, t_waiter *waiter);
+void				*ft_calloc(size_t count, size_t size);
+t_msg				*fill_msg();
+void				usleep_eat(t_waiter *waiter);
+void				usleep_sleep(t_waiter *waiter);
+void				usleep_ntime(int n);
+int					left(int x, int y);
 
 #endif
