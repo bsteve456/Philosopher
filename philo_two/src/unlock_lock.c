9eaf84f13383687b0520b9c8daaeb4e4fd31fe6a
bsteve456 <stevealan1@protonmail.com>
2020-12-14 15:35:53 +0100

    modify philo two

--- philo_two/src/unlock_lock.c
+++ philo_two/src/unlock_lock.c
@@ -6,7 +6,7 @@
 /*   By: blacking <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 18:02:58 by blacking          #+#    #+#             */
-/*   Updated: 2020/11/10 15:36:32 by stbaleba         ###   ########.fr       */
+/*   Updated: 2020/12/14 14:53:25 by stbaleba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ int		lock_fork(t_waiter *waiter)
 
 int		lock_fork2(t_waiter *waiter)
 {
+	waiter->last_eat[waiter->id - 1] = utime();
+	waiter->nb_eat[waiter->id - 1] += 1;
 	waiter->msg[waiter->id - 1][waiter->j].msg = 2;
 	waiter->msg[waiter->id - 1][waiter->j].time = utime();
 	waiter->j += 1;
@@ -40,8 +42,6 @@ int		lock_fork2(t_waiter *waiter)
 
 int		unlock_fork(t_waiter *waiter)
 {
-	waiter->last_eat[waiter->id - 1] = utime();
-	waiter->nb_eat[waiter->id - 1] += 1;
 	if (waiter->ntoeat != -1 &&
 	waiter->nb_eat[waiter->id - 1] == waiter->ntoeat)
 		return (1);
